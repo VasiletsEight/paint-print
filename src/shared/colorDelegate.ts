32fc82f3531482a2ea9dd44c2895ee8390c6c47b
vasilets.eight <vasilets.seven@gmail.com>
2022-05-23 22:30:57 +0300

    return old realization

--- src/shared/colorDelegate.ts
+++ src/shared/colorDelegate.ts
@@ -1,31 +1,27 @@
-import {AllColors, ColorDelegate, ColorOutput, ColorSelf} from "../modules/color/color.types";
+import {AllColors, ColorDelegate, ColorOutput} from "../modules/color/color.types";
 import colors from "../modules/color/color.constant";
 import {output} from "./output";
+import {createProxy} from "./createPrxoy";
 
 export const paint = (function (): ColorDelegate {
     const buffer:string[] = []
     const {fg, bg, reset, ...remainingColors} = colors;
     const allColors: AllColors = {...fg, ...bg, ...remainingColors};
 
-    const colorOutput:ColorOutput = function (this: ColorSelf, value: unknown) {
-        const __buffer = structuredClone(buffer)
+    const colorOutput:ColorOutput = function (this: any, value: unknown) {
+        const clone = structuredClone(buffer);
         buffer.length = 0;
 
-        return output.call({__buffer}, value)
+        return output.call({__buffer:clone}, value)
     }
-    const compareFuncColor = Object.assign(colorOutput, allColors) as ColorDelegate;
+    const compareFuncColor = Object.assign(colorOutput, allColors, {__buffer:[]}) as any;
 
-    const proxySet = (target:any, key:string, value:string) => {target[key] = value; return true};
-    const proxyGet = (target: ColorDelegate, key: string, receiver:any) => {
-        buffer.push(target[key])
+    const proxySet = () => {return false};
+    const proxyGet = (target:any, key: string, receiver:any):any => {
+        buffer.push(target[key]);
+
         return receiver
-
     };
 
-    const proxyHandler = {
-        get: proxyGet,
-        set: proxySet
-    }
-
-    return new Proxy(compareFuncColor, proxyHandler);
+    return createProxy(compareFuncColor, proxyGet, proxySet);
 }())
