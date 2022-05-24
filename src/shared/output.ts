87b9cd114fe2e28a766612125d0c790ea3786887
vasilets.eight <vasilets.seven@gmail.com>
2022-05-24 11:56:41 +0300

    add type to output function

--- src/shared/output.ts
+++ src/shared/output.ts
@@ -1,8 +1,8 @@
-import {ColorSelf} from "../modules/color/color.types";
+import {ColorOutput, ColorSelf} from "../modules/color/color.types";
 import colors from "../modules/color/color.constant";
 
 const {reset} = colors;
 
-export function output (this:ColorSelf, value:unknown) {
+export const output:ColorOutput =  function (this:ColorSelf, value:unknown) {
     return [...this.__buffer, value, reset].join("");
 }
