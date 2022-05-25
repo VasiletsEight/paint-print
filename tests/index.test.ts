aaa26bba9f6d93894305c8470d6202cd873df4f1
vasilets.eight <vasilets.seven@gmail.com>
2022-05-25 22:58:39 +0300

    update tets

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,49 +1,17 @@
 import {describe, it} from "mocha"
+import {paintTest} from "./shared/paint.test";
+import {redefineTest} from "./shared/redefine.test";
 import {paint} from "../src";
-import {expect} from "chai";
-import {redefineError, redefineInfo, redefineWarn} from "../src/shared";
 import {anotherPaint} from "../src/shared/colorDelegateChalk";
 
-describe("Test paint-print", function() {
-    it("Test paint", function() {
-        console.time("Default")
-        const warning = paint.yellow;
-        const successful = paint.green
-        const error = paint.red;
+describe("Test paint-print", function () {
+    before("Before Test paint", paintTest(paint, "Default paint"));
+    before("Before Test another paint", paintTest(anotherPaint, "Another paint"));
 
-        console.log(paint.blackBg.red("Hello world!!!"), "Default");
-        console.info(paint.black("Hello world!!!"), "Default")
-        console.warn(warning("Hello world"), "Default");
-        console.log(successful("Hello world"),  "Default");
-        console.timeEnd("Default")
+    it("It Test paint", paintTest(paint, "Default paint"));
+    it("It Test another paint", paintTest(anotherPaint, "Another paint"));
+    it("It Test redefine", redefineTest);
 
-        expect(successful("Default")).to.equal("\x1b[32mDefault\x1b[0m");
-    });
-
-    it("Test another paint", function() {
-        console.time("AnotherPaint")
-        const warning = anotherPaint.yellow;
-        const successful = anotherPaint.green
-        const error = anotherPaint.red;
-
-        console.log(anotherPaint.blackBg.red("Hello world!!!"), "AnotherPaint");
-        console.info(anotherPaint.black("Hello world!!!"), "AnotherPaint")
-        console.warn(warning("Hello world"), "AnotherPaint");
-        console.log(successful("Hello world"),  "AnotherPaint");
-        console.timeEnd("AnotherPaint")
-    });
-
-    it("Test redefine", function() {
-        console.warn("Warning");
-        console.error("Error");
-        console.info("Info");
-
-        redefineWarn();
-        redefineError();
-        redefineInfo();
-
-        console.warn("Warning");
-        console.error("Error");
-        console.info("Info");
-    });
+    after("After Test paint", paintTest(paint, "Default paint"));
+    after("After Test another paint", paintTest(anotherPaint, "Another paint"));
 });
