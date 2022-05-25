bf41b4c3417f52249ccd83747531ed4b7353076d
vasilets.eight <vasilets.seven@gmail.com>
2022-05-25 14:48:49 +0300

    refactor function colorDelegate interface,instead of __buffer use _buffer

--- src/shared/colorDelegate.ts
+++ src/shared/colorDelegate.ts
@@ -2,21 +2,22 @@ import {AllColors, ColorDelegate, ColorSelf} from "../modules/color/color.types"
 import colors from "../modules/color/color.constant";
 import {output} from "./output";
 import {createProxy} from "./createPrxoy";
+import {ProxyBoundArgs} from "../modules/proxy/proxy.type";
 
-export const paint = (function (): ColorDelegate {
+export const colorDelegate = (): ColorDelegate => {
     const {fg, bg, reset, ...remainingColors} = colors;
     const allColors: AllColors = {...fg, ...bg, ...remainingColors};
 
-    const compareFuncColor = (buffer:string[] = [])=> {
-        return Object.assign(output.bind({__buffer:buffer}), allColors) as ColorDelegate;
+    const compareFuncColor = (buffer: string[] = []): ColorDelegate => {
+        return Object.assign(output.bind({_buffer: buffer}), allColors) as ColorDelegate;
     }
 
     const proxySet = () => false;
     const proxyGet = function (this: ColorSelf, target: ColorDelegate, key: string): ColorDelegate {
-        const __buffer = [...this.__buffer, target[key]];
+        const _buffer = [...this._buffer, target[key]];
 
-        return createProxy(compareFuncColor(__buffer), proxyGet, proxySet, {__buffer});
+        return createProxy.call<ColorSelf, ProxyBoundArgs, ColorDelegate>({_buffer}, compareFuncColor(_buffer), proxyGet, proxySet);
     };
 
-    return createProxy(compareFuncColor(), proxyGet, proxySet, {__buffer: []});
-}())
+    return createProxy.call<ColorSelf, ProxyBoundArgs, ColorDelegate>({_buffer: []}, compareFuncColor(), proxyGet, proxySet);
+}
