881e65911b17295dd7f59a099e3297b616d0ed8b
vasilets.eight <vasilets.seven@gmail.com>
2022-05-25 14:45:10 +0300

    refactor imports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-export * from "./shared/index";
+import {colorDelegate} from "./shared";
+
+export {redefineInfo, redefineWarn, colorDelegate} from "./shared"
 export * from "./modules/index";
+
+export const paint = colorDelegate();
