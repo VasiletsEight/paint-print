328e72170bba42484545733a7891d8ab0987c206
vasilets.eight <vasilets.seven@gmail.com>
2022-05-25 15:08:12 +0300

    colorDelegateChalk.ts make function static

--- src/shared/colorDelegateChalk.ts
+++ src/shared/colorDelegateChalk.ts
@@ -5,19 +5,17 @@ import {output} from "./output";
 import {definePropertyObject} from "./definePropertyObject";
 import {DefinePropertyBound} from "../modules/defineProperty/defineProperty.type";
 
-const {fg, bg ,reset, ...remainingColors} = colors;
-const allColors:AllColors = {...fg, ...bg, ...remainingColors};
+const {fg, bg, reset, ...remainingColors} = colors;
+const allColors: AllColors = {...fg, ...bg, ...remainingColors};
 
-function colorDelegateChalk (buffer:string[]=[]):ColorDelegate {
-    const colorOutput = output.bind({_buffer:buffer}) as ColorDelegate;
+export const anotherPaint = (function colorDelegateChalk(buffer: string[] = []): ColorDelegate {
+    const colorOutput = output.bind({_buffer: buffer}) as ColorDelegate;
 
-    const getColor = function(this:DefinePropertyBound<ColorDelegate>){
+    const getColor = function (this: DefinePropertyBound<ColorDelegate>) {
         return colorDelegateChalk([...buffer, allColors[this.key]]);
     }
 
     definePropertyObject(colorOutput, Object.keys(allColors), getColor)
 
     return colorOutput;
-}
-
-export const anotherPaint = colorDelegateChalk();
+}());
