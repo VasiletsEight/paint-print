18c3ae3b296e91ea62057d7337722c92126f97d5
vasilets.eight <vasilets.seven@gmail.com>
2022-05-27 22:33:52 +0300

    delete unused function

--- src/modules/proxy/proxy.type.ts
+++
@@ -1,5 +0,0 @@
-import {ColorDelegate} from "../color/color.types";
-
-export type GetProxy<T> = (target:T, key:string, receiver:T)=>T;
-export type SetProxy = ()=>boolean;
-export type ProxyBoundArgs = [ColorDelegate, GetProxy<ColorDelegate> ,()=>boolean];
