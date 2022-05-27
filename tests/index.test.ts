64e6c005d011407db0ee0d1bed636d9da7ac2a62
vasilets.eight <vasilets.seven@gmail.com>
2022-05-27 22:32:38 +0300

    create root function

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,17 +1,11 @@
-import {describe, it} from "mocha"
+import {describe} from "mocha"
 import {paintTest} from "./shared/paint.test";
-import {redefineTest} from "./shared/redefine.test";
 import {paint} from "../src";
-import {anotherPaint} from "../src/shared/colorDelegateChalk";
 
 describe("Test paint-print", function () {
-    before("Before Test paint", paintTest(paint, "Default paint"));
-    before("Before Test another paint", paintTest(anotherPaint, "Another paint"));
+    beforeEach("Before Test paint", (done) => paintTest(done, paint, "After Default paint"));
 
-    it("It Test paint", paintTest(paint, "Default paint"));
-    it("It Test another paint", paintTest(anotherPaint, "Another paint"));
-    it("It Test redefine", redefineTest);
+    Array.from({length: 10}, (_, i) => i).forEach(() => it("It Test paint", (done) => paintTest(done, paint, "It Default paint")))
 
-    after("After Test paint", paintTest(paint, "Default paint"));
-    after("After Test another paint", paintTest(anotherPaint, "Another paint"));
+    afterEach("After Test paint", (done) => paintTest(done, paint, "After Default paint"));
 });
