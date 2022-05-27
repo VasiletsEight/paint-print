34732f2006dccf922e5ea195835c0b4af55eb5c0
vasilets.eight <vasilets.seven@gmail.com>
2022-05-27 22:56:52 +0300

    change test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,13 +1,11 @@
-import {describe, Done} from "mocha"
+import {describe} from "mocha"
 import {paintTest} from "./shared/paint.test";
 import {paint} from "../src";
 
-const testInput = (done: Done) => paintTest(done, paint, "After Default paint")
-
 describe("Test paint-print", function () {
-    beforeEach("Before Test paint", testInput);
+    beforeEach("Before Test paint", paintTest(paint, "Before paint paint"));
 
-    Array.from(Array(10).keys()).forEach(() => it("It Test paint", testInput))
+    Array.from(Array(10).keys()).forEach(() => it("It Test paint", paintTest(paint, "It paint paint")))
 
-    afterEach("After Test paint", testInput);
+    afterEach("After Test paint", paintTest(paint, "After paint paint"));
 });
